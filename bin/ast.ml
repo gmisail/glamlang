eac3c23bc246eb7101a47770eaf54074f572036f
Graham Misail <gmisail64@gmail.com>
2022-05-29 17:02:25 -0400

    Defined AST foundation

--- bin/ast.ml
+++ bin/ast.ml
@@ -1,6 +1,16 @@
-(* use this as reference for this: https://craftinginterpreters.com/appendix-i.html, also the github for menhir lua *)
-
 type name = Name of string
+
+type primitive = 
+        | None
+        | String of string
+        | Int of int
+        | Float of float
+        | True
+        | False
+
+type variable_type = 
+        | name 
+        | Signature of name * list * variable_type (* (int, int) -> int *)
 
 type binary_op = 
         | Add
@@ -21,25 +31,25 @@ type unary_op =
         | LogicalNot
         | Increment
         | Decrement
+        | Negative
 
 type expression = 
-        | None
-        | False
-        | True
-        | Integer of int
-        | Float of float
-        | String of string
+        | Constant of constant
+        | Identifier of name
         | BinOp of binary_op * expression * expression 
         | UnOp of unary_op * expression
-
-type arguments = Arguments of expression list
-
-type block = statement list
-
-type statement = 
+        | FunctionCall of name * arguments
+        | FunctionDeclaration of (name * name) list * block 
+        | Index of expression * expression
+and arguments = expression list
+and block = statement list
+and statement = 
         | Assignment of name * expression
-        | FunctionCall of name * arguments 
-        | While of expression * block
+        | SignatureDefinition of name * variable_type   (* define square: (int) -> int *)
+        | VariableDeclaration of name * variable_type * expression option
+        | While of expression * statement 
         | If of (expression * block) list * block option
         | For of name * expression              (* for var in expr { ... } *) 
-
+        | Return of expression
+        | Block of block
+        | Eval of expression
