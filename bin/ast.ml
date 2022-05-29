26812095f92f5b170897e339a575f39775f5ed0d
Graham Misail <gmisail64@gmail.com>
2022-05-29 15:41:25 -0400

    Fleshed out AST types

--- bin/ast.ml
+++ bin/ast.ml
@@ -1,14 +1,45 @@
-type expr = 
-        | Atom of string                        (* hello, world   *)
-        | Float of float                        (* 100.0          *)
-        | Int of int                            (* 100            *)
-        | Bool of bool                          (* true, false    *)
-        | Assignment of string * expr           (* a = b          *)
-        | FunctionCall of string * expr list    (* function(a, b) *)
+(* use this as reference for this: https://craftinginterpreters.com/appendix-i.html, also the github for menhir lua *)
+
+type name = Name of string
+
+type binary_op = 
+        | Add
+        | Sub
+        | Mul
+        | Div
+        | Mod
+        | And
+        | Or
+        | Equality
+        | Inequality
+        | Less
+        | Greater
+        | LessEqual
+        | GreaterEqual
+
+type unary_op = 
+        | LogicalNot
+        | Increment
+        | Decrement
+
+type expression = 
+        | None
+        | False
+        | True
+        | Integer of int
+        | Float of float
+        | String of string
+        | BinOp of binary_op * expression * expression 
+        | UnOp of unary_op * expression
+
+type arguments = Arguments of expression list
+
+type block = statement list
 
 type statement = 
-        | Expr of expr
-        | VariableDeclaration of string * string * expr         (* let x: int = 100 *)
-        | VariableDeclarationExpression of string * string      (* let x: int       *)
+        | Assignment of name * expression
+        | FunctionCall of name * arguments 
+        | While of expression * block
+        | If of (expression * block) list * block option
+        | For of name * expression              (* for var in expr { ... } *) 
 
-type program = Program of atom list 
